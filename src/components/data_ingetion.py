168c58c8195bf2b9973ef4b8dc6e03e69053b1e4
Preetam Rana <pritamrana770@gmail.com>
2023-07-16 21:44:34 +0530

    data transformation completed

--- src/components/data_ingetion.py
+++ src/components/data_ingetion.py
@@ -4,7 +4,11 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from dataclasses import dataclass 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig 
+
 
 
 @dataclass
@@ -43,4 +47,9 @@ class DataIngetion:
         
 if __name__=='__main__':
     obj = DataIngetion()
-    obj.initiate_data_ingetion() 
+    train_data,test_data = obj.initiate_data_ingetion()  
+
+    data_transformation =DataTransformation() 
+    data_transformation.initiate_data_transformation(train_data,test_data)  
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data) 
+    
